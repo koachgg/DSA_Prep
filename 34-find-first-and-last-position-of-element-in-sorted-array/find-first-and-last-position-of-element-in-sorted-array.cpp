d9193c428b60a59ec4465d669f5529f1ec6f5040
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-03-09 14:43:18 +0530

    Time: 3 ms (89.79%) | Memory: 16.4 MB (10.27%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+     int first = firstOccurrence(nums,target);
+     if(first==-1) return {-1,-1};
+     int last = lastOccurence(nums,target);
+     if(last==-1) return {-1,-1};
+     return {first,last};   
+    }
+
+private:    
+    int firstOccurrence(vector<int>&nums,int target){
+        int low = 0;
+        int high = nums.size()-1;
+        int first = -1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                first = mid;
+                high = mid-1;
+            }
+            else if(nums[mid]<target){
+                low = mid + 1;
+            }
+            else {
+                high = mid -1;
+            }
+        }
+        return first;
+    }
+    int lastOccurence(vector<int> nums,int target){
+        int low = 0;
+        int high = nums.size()-1;
+        int ans = -1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid]<target){
+                low = mid +1;
+            }
+            else {
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+};
