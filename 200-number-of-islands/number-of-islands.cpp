6fd1ca9a2745ae44c620865369769c5176ee5d27
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-04-13 22:12:56 +0530

    Time: 60 ms (11.45%) | Memory: 24 MB (14.48%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,41 @@
+class Solution {
+
+private :
+        void dfs(int row,int col,vector<vector<char>> &grid,vector<vector<int>> &vis,int row0,int col0){
+        int n = grid.size();
+        int m = grid[0].size();
+        vis[row][col] = 1;
+            // vec.push_back({row-row0,col-col0})
+        vector<int>delrow{-1,0,1,0};
+        vector<int>delcol{0,1,0,-1};
+        for (int i = 0; i < 4; i++) {
+        int nrow = row + delrow[i];
+        int ncol = col + delcol[i];
+        // check for valid unvisited land neighbour coordinates 
+        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+          !vis[nrow][ncol] && grid[nrow][ncol] == '1') {
+          dfs(nrow, ncol,grid, vis,row0, col0);
+        }
+      }
+        }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n,vector<int> (m,0));
+        set<vector<pair<int,int>>> st;
+        int count = 0;
+
+        for(int i =0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    vector<pair<int,int>> vec;
+                    dfs(i,j,grid,vis,i,j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
