1d1614796c9d088caecf654784b1d65e1014f0ef
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-06-07 20:19:33 +0530

    Time: 4 ms (67.72%) | Memory: 14.7 MB (30.42%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -0,0 +1,70 @@
+// class Solution {
+// public:
+//     void solve(string ip, string op, vector<string> &v)
+//     {
+//         if(ip.length()==0){
+//             v.push_back(op);
+//         }
+//         if(isalpha(ip[0])){
+//             string op1 = op;
+//             string op2 = op;
+
+//             op1.push_back(tolower(ip[0]));
+//             op2.push_back(toupper(ip[0]));
+
+//             ip.erase(ip.begin()+0);
+//             solve(ip,op1,v);
+//             solve(ip,op2,v);
+//         }
+
+//         else {
+//             string op1 = op;
+//             op1.push_back(ip[0]);
+//             ip.erase(ip.begin()+0);
+//             solve(ip,op1,v);
+//         }
+//     }
+//     vector<string> letterCasePermutation(string s) {
+//         string ip = s;
+//         string op = "";
+//         vector<string> ans;
+//         solve(ip,op,ans);
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+    void solve(string ip, string op, vector<string> &v)
+    {
+        if (ip.length() == 0) {
+            v.push_back(op);
+            return;
+        }
+
+        if (isalpha(ip[0])) {
+            string op1 = op;
+            string op2 = op;
+
+            op1.push_back(tolower(ip[0]));
+            op2.push_back(toupper(ip[0]));
+
+            ip.erase(ip.begin() + 0);  // Create a copy to pass to recursive calls
+            solve(ip, op1, v);
+            solve(ip, op2, v);
+        } else {
+            string op1 = op;
+            op1.push_back(ip[0]);
+            ip.erase(ip.begin() + 0);  // Create a copy to pass to recursive calls
+            solve(ip, op1, v);
+        }
+    }
+
+    vector<string> letterCasePermutation(string s) {
+        string ip = s;
+        string op = "";
+        vector<string> ans;
+        solve(ip, op, ans);
+        return ans;
+    }
+};
