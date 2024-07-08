a4c6f94a4d30ef8e25d07607c7346e781d75a081
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-07-09 01:50:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (22.16%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,22 +1,35 @@
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+//         double ans = 1.0;
+//         long long nn = n;
+//         if(nn<0){
+//             nn = -1 * nn;
+//         }
+//         while(nn>0){
+//             if(nn%2==1){
+//             ans = ans * x;
+//             nn = nn-1;
+//         }
+//         else {
+//             x = x*x;
+//             nn = nn/2;
+//         }
+//         }
+//         if(n<0) ans = (double)(1.0) / (double)(ans);
+//         return ans;
+//     }   
+// };
+
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans = 1.0;
-        long long nn = n;
-        if(nn<0){
-            nn = -1 * nn;
+        if(n==0){
+            return 1;
         }
-        while(nn>0){
-            if(nn%2==1){
-            ans = ans * x;
-            nn = nn-1;
+        if(n==1){
+            return x;
         }
-        else {
-            x = x*x;
-            nn = nn/2;
-        }
-        }
-        if(n<0) ans = (double)(1.0) / (double)(ans);
-        return ans;
-    }   
+        return pow(x,n);
+    }
 };
