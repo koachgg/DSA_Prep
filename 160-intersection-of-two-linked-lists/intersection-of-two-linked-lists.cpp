1e0a9fa4c5cb4e078f42d6cb8a29336f2afe28a4
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-07-19 23:32:51 +0530

    Time: 34 ms (85.93%) | Memory: 17.4 MB (26.43%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,15 +9,31 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        while(headB!=NULL){
-        ListNode* temp = headA;
-        while(temp!=NULL){
-            if(temp==headB) return headB;
+        // Brute Force
+        // while(headB!=NULL){
+        // ListNode* temp = headA;
+        // while(temp!=NULL){
+        //     if(temp==headB) return headB;
 
-            temp=temp->next;
+        //     temp=temp->next;
+        // }
+        // headB=headB->next;
+        // }
+        // return NULL;
+        // Better Approach : hashing use a set/unordered_set
+        // Optimal : Difference in Length
+    if(headA==NULL || headB==NULL) return NULL;
+        ListNode* d1 = headA;
+        ListNode* d2 = headB;
+
+        while(d1!=d2){
+            if(d1==NULL) d1=headB;
+            else d1=d1->next;
+
+            if(d2==NULL) d2=headA;
+            else d2=d2->next;
         }
-        headB=headB->next;
-        }
-        return NULL;
+        return d1;
     }
 };
+
