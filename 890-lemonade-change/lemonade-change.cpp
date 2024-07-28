9a3bb8cc6d54645eed42377651bf85d5d217cac5
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-07-28 16:44:26 +0530

    Time: 76 ms (40.01%) | Memory: 86.1 MB (18.82%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,54 @@
+// class Solution {
+// public:
+//     bool lemonadeChange(vector<int>& bills) {
+//         int five = 0;
+//         int ten = 0;
+
+//         for(int i=0;i<bills.size();i++){
+//             if(bills[i]==5) five++;
+//             else if(bills[i]==10)
+//             {
+//                 if(five) five--; ten++;
+//                 else return false;
+//             }
+//             else {
+//                 if(ten && five) ten--; five--;
+//                 else if(five>=3) five -=3;
+//                 else  return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int i = 0; i < bills.size(); i++) {
+            if (bills[i] == 5) {
+                five++;
+            } else if (bills[i] == 10) {
+                if (five) {
+                    five--;
+                    ten++;
+                } else {
+                    return false;
+                }
+            } else { // bills[i] == 20
+                if (ten && five) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
