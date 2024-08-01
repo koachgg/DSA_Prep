92286acd210d16c6fd6ae47751830759c3ffa341
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-08-01 19:54:24 +0530

    Time: 3 ms (29.63%) | Memory: 7.1 MB (91.31%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -12,17 +12,39 @@
 // };
 
 
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         vector<int> dp(n + 1, 0);
+
+//         dp[0] = 1;
+//         dp[1] = 1;
+
+//         for(int i = 2; i <= n; i++){
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+//         return dp[n];
+//     }
+// };
+
 class Solution {
 public:
-    int climbStairs(int n) {
-        vector<int> dp(n + 1, 0);
+    int climbStairs(int n) 
+    {
+        if(n<=2)
+            return n;
 
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for(int i = 2; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+      int n1=1;
+      int n2=2;
+      int n3=0;
+     
+        for(int i=2;i<n;i++)
+        {
+            n3=n1+n2;  //solved using fibonacci approch
+            n1=n2;
+            n2=n3;
         }
-        return dp[n];
+        return n3;
+
     }
 };
