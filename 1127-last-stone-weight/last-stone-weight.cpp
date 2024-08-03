7b954435a0bf29e5f23c8265d408965aa41658cc
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-08-03 22:53:39 +0530

    Time: 4 ms (36.89%) | Memory: 9.5 MB (23.62%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,20 +1,36 @@
+// class Solution {
+// public:
+//     int lastStoneWeight(vector<int>& stones) {
+//         priority_queue<int>pq;
+
+//         for(int &stone:stones){
+//             pq.push(stone);
+//         }
+
+//         while(pq.size()>1){
+//             int a = pq.top();
+//             pq.pop();
+//             int b = pq.top();
+//             pq.pop();
+
+//             pq.push(abs(a-b));
+//         }
+//         return pq.top();
+//     }
+// };
+
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int>pq;
+        while(stones.size()>1){
+            sort(begin(stones),end(stones));
+            int a = stones.back();
+            stones.pop_back();
+            int b = stones.back();
+            stones.pop_back();
 
-        for(int &stone:stones){
-            pq.push(stone);
+            stones.push_back(abs(a-b));
         }
-
-        while(pq.size()>1){
-            int a = pq.top();
-            pq.pop();
-            int b = pq.top();
-            pq.pop();
-
-            pq.push(abs(a-b));
-        }
-        return pq.top();
+        return stones[0];
     }
 };
