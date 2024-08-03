2300342f91faecf3fd92069b26614a5a63d89b26
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-08-03 22:54:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (46.08%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,36 +1,38 @@
-// class Solution {
-// public:
-//     int lastStoneWeight(vector<int>& stones) {
-//         priority_queue<int>pq;
-
-//         for(int &stone:stones){
-//             pq.push(stone);
-//         }
-
-//         while(pq.size()>1){
-//             int a = pq.top();
-//             pq.pop();
-//             int b = pq.top();
-//             pq.pop();
-
-//             pq.push(abs(a-b));
-//         }
-//         return pq.top();
-//     }
-// };
-
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        while(stones.size()>1){
-            sort(begin(stones),end(stones));
-            int a = stones.back();
-            stones.pop_back();
-            int b = stones.back();
-            stones.pop_back();
+        priority_queue<int>pq;
 
-            stones.push_back(abs(a-b));
+        for(int &stone:stones){
+            pq.push(stone);
         }
-        return stones[0];
+
+        while(pq.size()>1){
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+
+            pq.push(abs(a-b));
+        }
+        return pq.top();
     }
 };
+
+
+// Brute Force
+// class Solution {
+// public:
+//     int lastStoneWeight(vector<int>& stones) {
+//         while(stones.size()>1){
+//             sort(begin(stones),end(stones));
+//             int a = stones.back();
+//             stones.pop_back();
+//             int b = stones.back();
+//             stones.pop_back();
+
+//             stones.push_back(abs(a-b));
+//         }
+//         return stones[0];
+//     }
+// };
