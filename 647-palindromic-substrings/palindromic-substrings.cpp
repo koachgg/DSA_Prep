a7409d98faeb26ab4cfe9c7d38d827d11de61833
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-08-11 00:45:37 +0530

    Time: 3 ms (95.54%) | Memory: 21.5 MB (33.75%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,27 +1,51 @@
+// class Solution {
+// public:
+//     int countSubstrings(string s) {
+//        int res = 0;
+//        for(int i=0;i<s.size();i++){
+//         int l;
+//         int r;
+//         l=r=i;
+//         // For ODD
+//         while(l>=0 && r<s.size() && s[l]==s[r]){
+//             res +=1;
+//             l--;
+//             r++;
+//         }
+//         // For Even
+//         l = i;
+//         r = i+1;
+
+//         while(l>=0 && r<s.size() && s[l]==s[r]){
+//             res +=1;
+//             l--;
+//             r++;
+//         }
+
+//        }
+//        return res; 
+//     }
+// };
+
 class Solution {
 public:
     int countSubstrings(string s) {
-       int res = 0;
-       for(int i=0;i<s.size();i++){
-        int l;
-        int r;
-        l=r=i;
-        while(l>=0 && r<s.size() && s[l]==s[r]){
-            res +=1;
+        int res = 0;
+        for (int i = 0; i < s.size(); i++) {
+            res += countPali(s, i, i);
+            res += countPali(s, i, i + 1);
+        }
+        return res;
+    }
+
+private:
+    int countPali(string s, int l, int r) {
+        int res = 0;
+        while (l >= 0 && r < s.size() && s[l] == s[r]) {
+            res++;
             l--;
             r++;
         }
-
-        l = i;
-        r = i+1;
-
-        while(l>=0 && r<s.size() && s[l]==s[r]){
-            res +=1;
-            l--;
-            r++;
-        }
-
-       }
-       return res; 
+        return res;
     }
 };
