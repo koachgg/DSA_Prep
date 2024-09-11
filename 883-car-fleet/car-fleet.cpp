d36645e5a05f05c8d0ab3a0924a9a3ba4c7698d7
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-09-11 12:16:51 +0530

    Time: 684 ms (5.04%) | Memory: 176.8 MB (5.06%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -1,33 +1,63 @@
-// class Solution {
-// public:
-//     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        
-//     }
-// };
-
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        int n = position.size();
-        vector<pair<double, double>> pairs(n);
-        for (int i = 0; i < n; i++) {
-            pairs[i] = { position[i], speed[i] };
-        }
-        sort(pairs.begin(), pairs.end(), [](const pair<double, double>& a, const pair<double, double>& b) {
-            return b.first < a.first;
-        });
+     priority_queue<vector<double>> pq;
+     for(int i=0;i<position.size();i++){
+        double time = double(target - position[i])/speed[i];
 
+        pq.push({(double)position[i],(double)speed[i],time});
+     }   
+     if(pq.size()==0) return 0;
 
-        int fleetCount = 0;
-        vector<double> timeToReach(n);
-        for (int i = 0; i < n; i++) {
-            timeToReach[i] = (target - pairs[i].first) / pairs[i].second;
-            if (i >= 1 && timeToReach[i] <= timeToReach[i - 1]) {
-                timeToReach[i] = timeToReach[i - 1];
-            } else {
-                fleetCount++;
-            }
+     int fleet = 0;
+     while(true) {
+        if(pq.size()==1){
+            fleet++;
+            break;
         }
-        return fleetCount;
+        auto ahead = pq.top();
+        pq.pop();
+        auto behind = pq.top();
+        pq.pop();
+
+        if(ahead[2]>=behind[2]){
+            pq.push(ahead);
+        }
+        else {
+            fleet++;
+            pq.push(behind);
+        }
+     }
+     return fleet;
     }
 };
+
+// class Solution {
+// public:
+//     int carFleet(int target, vector<int>& position, vector<int>& speed) {
+//         int n = position.size();
+//         vector<pair<double, double>> pairs(n);
+//         for (int i = 0; i < n; i++) {
+//             pairs[i] = { position[i], speed[i] };
+//         }
+//         sort(pairs.begin(), pairs.end(), [](const pair<double, double>& a, const pair<double, double>& b) {
+//             return b.first < a.first;
+//         });
+
+
+//         int fleetCount = 0;
+//         vector<double> timeToReach(n);
+//         for (int i = 0; i < n; i++) {
+//             timeToReach[i] = (target - pairs[i].first) / pairs[i].second;
+//             if (i >= 1 && timeToReach[i] <= timeToReach[i - 1]) {
+//                 timeToReach[i] = timeToReach[i - 1];
+//             } else {
+//                 fleetCount++;
+//             }
+//         }
+//         return fleetCount;
+//     }
+// };
+
+// using priority queue 
+
