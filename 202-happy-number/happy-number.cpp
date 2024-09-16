db07d015daa3fa5e9ec12e2af875b857b2c37540
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-09-16 13:02:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (89.41%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,25 +1,51 @@
+// class Solution {
+// public:
+//     bool isHappy(int n) {
+//        unordered_set<int> visit;
+
+//        while(visit.find(n)==visit.end()){
+//         visit.insert(n);
+//         n = getNextNumber(n);
+//         if(n==1){
+//             return true;
+//         }
+//        } 
+//        return false;
+//     }
+
+//     int getNextNumber(int n){
+//         int output = 0;
+
+//         while(n>0){
+//             int digit = n%10;
+//             output += digit * digit;
+//             n = n/10;
+//         }
+//         return output;
+//     }
+// };
+
+
 class Solution {
 public:
     bool isHappy(int n) {
-       unordered_set<int> visit;
+        int slow = n, fast = sumSquareDigits(n);
 
-       while(visit.find(n)==visit.end()){
-        visit.insert(n);
-        n = getNextNumber(n);
-        if(n==1){
-            return true;
+        while (slow != fast) {
+            fast = sumSquareDigits(fast);
+            fast = sumSquareDigits(fast);
+            slow = sumSquareDigits(slow);
         }
-       } 
-       return false;
+
+        return fast == 1;
     }
 
-    int getNextNumber(int n){
+private:
+    int sumSquareDigits(int n) {
         int output = 0;
-
-        while(n>0){
-            int digit = n%10;
-            output += digit * digit;
-            n = n/10;
+        while (n != 0) {
+            output += (n % 10) * (n % 10);
+            n /= 10;
         }
         return output;
     }
