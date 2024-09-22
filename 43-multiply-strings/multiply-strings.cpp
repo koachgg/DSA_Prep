d92c4a7543a4a31fcd3a142465ca33ddbfb01d13
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-09-22 11:17:48 +0530

    Time: 8 ms (25.76%) | Memory: 8.5 MB (69.94%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if(num1 == "0" || num2 == "0") return "0";
+
+        vector<int> num(num1.size()+num2.size(),0);
+        // main part
+        
+        for(int i=num1.size()-1;i>=0;i--){
+            for(int j = num2.size()-1;j>=0;j--){
+
+                num[i+j+1] += (num1[i]-'0') * (num2[j]-'0');
+                num[i+j] += num[i+j+1]/10;
+                num[i+j+1] = num[i+j+1]%10;
+            }
+        }
+
+        // skip leading zeros
+        int i=0;
+        while(i<num.size()&&num[i]==0){
+            ++i;
+        }
+        // transforming the vector into a string 
+        string res = "";
+        while(i<num.size()){
+            res.push_back(num[i++]+'0');
+        }
+        return res;
+    }
+};
